daff360c9bcd72e0c9e070500935f3c844369799
Bob <rpmullaly@gmail.com>
2024-11-28 12:48:34 -0800

    Added HTML, CSS, and JS files and code

---
+++ basic/scripts.js
@@ -0,0 +1,76 @@
+import qr from "https://cdn.jsdelivr.net/npm/qr-code-styling@1.6.0-rc.1/+esm";
+import feather from "https://cdn.jsdelivr.net/npm/feather-icons@4.29.2/+esm";
+feather.replace();
+
+let address = "tb1q6th5dqvtum6khn77f2p33vlkyk40tdzkkp5mn8"; // edit to your address
+
+let options = {
+  width: 222,
+  height: 222,
+  type: "svg",
+  data: address,
+  dotsOptions: {
+    color: "hotpink",
+    type: "dots"
+  },
+  cornersSquareOptions: {
+    type: "dot"
+  },
+  cornersDotOptions: {
+    type: "dot"
+  },
+  backgroundOptions: {
+    color: null
+  },
+  imageOptions: {
+    crossOrigin: "anonymous",
+    margin: 20
+  }
+};
+
+let qrCode = new qr(options);
+
+qrCode.append(document.getElementById("qr-code"));
+
+let addressElem = document.getElementById("address");
+addressElem.innerHTML = truncateAddress(address);
+
+document.getElementById("copy-btn").addEventListener("click", copyAddress);
+
+function copyAddress() {
+  const button = document.getElementById("copy-btn");
+  button.disabled = true; // Disable the button
+  navigator.clipboard
+    .writeText(address)
+    .then(() => {
+      addressElem.innerHTML = "Address Copied ✅";
+
+      setTimeout(() => {
+        addressElem.textContent = truncateAddress(address);
+        button.disabled = false; // Re-enable the button after reset
+      }, 2222);
+    })
+    .catch((err) => {
+      console.log("Something went wrong", err);
+      button.disabled = false; // Re-enable in case of an error
+    });
+}
+
+function truncateAddress(address) {
+  const screenWidth = window.innerWidth;
+
+  if (screenWidth < 500) {
+    // For narrow screens, show the truncated address
+    return address.slice(0, 6) + "..." + address.slice(-6);
+  } else {
+    // Return full address for wider screens
+    return address;
+  }
+}
+
+function updateAddress() {
+  addressElem.textContent = truncateAddress(address);
+}
+
+window.addEventListener("resize", updateAddress);
+window.addEventListener("load", updateAddress);
